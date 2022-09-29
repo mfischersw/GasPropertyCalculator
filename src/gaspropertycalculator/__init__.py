91165cabde4c37df1984768dd351fa97d07d8bda
Michael Fischer <michael.fischer3@gmx.de>
2022-09-30 00:47:11 +0200

    migration

--- src/gaspropertycalculator/__init__.py
+++ src/gaspropertycalculator/__init__.py
@@ -1,6 +1,11 @@
 # This file is part of GasPropertyCalculator.
 
 from . import ui
+from . import dataGui
 from . import gaspropertycalculator
+from . import generics
+from . import parameters
+from . import pieChartGui
 
-__all__ = ['ui', 'gaspropertycalculator', ]
+__all__ = ['ui', 'dataGui', 'gaspropertycalculator', 'generics',
+           'parameters', 'pieChartGui', ]
